aca6962c010330de67c3505cdeb4471aa166402d
Michael Brown <michael.brown6@my.jcu.edu.au>
2011-06-17 16:10:35 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adventure::Application.config.secret_token = '55d9ffb8ed5df8bbcbc2e88c95a32c9abd583135d3fedd24172a84c1faf10f8bf8eda5d4e45f44cb4ff0fb5142e49a31a2701edcf980ec9281e10d77aff4ef11'

150205987807282fefa6d953ec37280df4a50a48
Michael Brown <michael.brown6@my.jcu.edu.au>
2011-06-29 18:14:31 +1000

    Added parent/child associations and tests

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,11 @@
 class Node < ActiveRecord::Base
-  attr_accessible :title, :body
+  attr_accessible :title, :body, :action_desc
 
-  validates_presence_of :title, :body
+  has_many :children, :class_name => "Node",
+                      :foreign_key => "parent_id"
+  belongs_to :parent, :class_name => "Node"
+
+  validates_presence_of :title, :body, :action_desc
   validates_length_of :title, :maximum => 40
   validates_length_of :body,  :maximum => 1000
 end
--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -2,11 +2,55 @@ require 'spec_helper'
 
 describe Node do
   before(:each) do
-    @attr = { :title => "Node title", :body => "Funny story" }
+    @attr = { :title => "Node title",
+              :body => "Funny story",
+              :action_desc => "woke up on the wrong side of the bed" }
   end
 
-  it "should create a new node" do
+  it "should create a new node given valid attributes" do
     Node.create!(@attr)
+  end
+
+  describe "associations" do
+    before(:each) do
+      @root_node = Node.create(@attr)
+    end
+
+    it "should respond to children associations" do
+      @root_node.should respond_to :children
+    end
+
+    it "should respond to parent associations" do
+      @root_node.should respond_to :parent
+    end
+
+    it "shouldn't have any children if we don't create them" do
+      @root_node.children.should be_empty
+    end
+
+    it "shouldn't have a parent, since it's the root node" do
+      @root_node.parent.should be_nil
+    end
+
+    it "creating a valid child should alter the node count by one" do
+      lambda do
+        @root_node.children.create!(@attr)
+      end.should change(Node, :count).by(1)
+    end
+
+    it "creating an invalid child node should not work" do
+      lambda do
+        @root_node.children.create(@attr.merge(:body => ''))
+      end.should_not change(Node, :count)
+    end
+
+    it "should correctly fetch parent & child associations" do
+      @child = @root_node.children.create!( :title => "Child",
+                                            :body => "Yellow bulldozer",
+                                            :action_desc => "brush teeth" )
+      @child.parent.should == @root_node
+      @root_node.children[0].should == @child
+    end
   end
 
   describe "validations" do
@@ -28,6 +72,11 @@ describe Node do
 
     it "shouldn't allow bodies that are too long" do
       test_node = Node.new(:title => @attr[:title], :body => "a"*1001)
+      test_node.should_not be_valid
+    end
+
+    it "should require an action description" do
+      test_node = Node.new(@attr.merge(:action_desc => ''))
       test_node.should_not be_valid
     end
   end

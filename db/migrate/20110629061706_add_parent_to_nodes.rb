09c3ae6cf35383da4ae4a1bc1c5e078ebd7be59f
Michael Brown <michael.brown6@my.jcu.edu.au>
2011-06-29 16:29:31 +1000

    Added parent pointers to nodes migration

---
+++ db/migrate/20110629061706_add_parent_to_nodes.rb
@@ -0,0 +1,11 @@
+class AddParentToNodes < ActiveRecord::Migration
+  def self.up
+    add_column :nodes, :parent_id, :integer
+    add_column :nodes, :action_desc, :text
+  end
+
+  def self.down
+    remove_column :nodes, :action_desc
+    remove_column :nodes, :parent_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110617130157) do
+ActiveRecord::Schema.define(:version => 20110629061706) do
 
   create_table "nodes", :force => true do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id"
+    t.text     "action_desc"
   end
 
 end
